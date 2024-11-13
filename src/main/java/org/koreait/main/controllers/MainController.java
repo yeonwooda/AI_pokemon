d878cd525f0128fc9ad14c243dbb86e3f70ee109
yeonwooda <mcsun5@naver.com>
2024-11-13 17:20:47 +0900

    프로젝트 기본 구성

---
+++ src/main/java/org/koreait/main/controllers/MainController.java
@@ -0,0 +1,26 @@
+package org.koreait.main.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.koreait.global.libs.Utils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class MainController {
+
+    private final Utils utils;
+
+
+    @GetMapping
+    public String index(Model model) {
+
+        //return utils.tpl("main/index");
+        return "front/main/index";
+    }
+}
