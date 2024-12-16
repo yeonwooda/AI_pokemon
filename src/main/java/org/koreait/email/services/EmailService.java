676700409b82ec81ca3c6aa7c28445cb9d685202
yeonwooda <mcsun5@naver.com>
2024-12-16 17:08:35 +0900

    이메일 전송 기능 구현

---
+++ src/main/java/org/koreait/email/services/EmailService.java
@@ -0,0 +1,78 @@
+package org.koreait.email.services;
+
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.koreait.email.controllers.RequestEmail;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    /**
+     *
+     * @param form
+     * @param tpl : 템플릿 코드  email/{tpl}.html
+     * @param tplData : 템플릿에 전달하는 데이터(EL 속성으로 추가)
+     * @return
+     */
+    public boolean sendEmail(RequestEmail form, String tpl, Map<String, Object> tplData) {
+
+        try {
+            Context context = new Context();
+            tplData = Objects.requireNonNullElseGet(tplData, HashMap::new);
+
+            List<String> to = form.getTo();
+            List<String> cc = form.getCc();
+            List<String> bcc = form.getBcc();
+            String subject = form.getSubject();
+            String content = form.getContent();
+
+            tplData.put("to", to);
+            tplData.put("cc", cc);
+            tplData.put("bcc", bcc);
+            tplData.put("subject", subject);
+            tplData.put("content", content);
+
+            context.setVariables(tplData);
+
+            String html = templateEngine.process("email/" + tpl, context);
+
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
+            helper.setTo(form.getTo().toArray(String[]::new));
+
+            if (cc != null && !cc.isEmpty()) {
+                helper.setCc(cc.toArray(String[]::new));
+            }
+
+            if (bcc != null && !bcc.isEmpty()) {
+                helper.setBcc(bcc.toArray(String[]::new));
+            }
+
+            helper.setSubject(subject);
+            helper.setText(html, true);
+
+            javaMailSender.send(message);
+
+            return true;
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
