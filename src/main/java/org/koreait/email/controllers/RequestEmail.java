4ac360523cf7283bb8fce6a8c91d4319b103a95f
yeonwooda <mcsun5@naver.com>
2024-12-16 16:46:14 +0900

    이메일 전송 기능 구현

--- src/main/java/org/koreait/email/controllers/RequestEmail.java
+++ src/main/java/org/koreait/email/controllers/RequestEmail.java
@@ -7,5 +7,9 @@ import java.util.List;
 @Data
 public class RequestEmail {
     private List<String> to; // 받는 쪽 이메일
+    private List<String> cc; // 참조 
+    private List<String> bcc; // 숨은 참조
+    private String subject; // 메일 제목
+    private String content; // 메일 내용
 
 }
