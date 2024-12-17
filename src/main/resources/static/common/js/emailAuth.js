c3c1a2a2eaa2be7e839909ef2906c92882a4666c
yeonwooda <mcsun5@naver.com>
2024-12-17 16:07:21 +0900

    이메일 전송 기능 구현

---
+++ src/main/resources/static/common/js/emailAuth.js
@@ -0,0 +1,16 @@
+var commonLib = commonLib : {};
+
+// 이메일 인증 코드
+commonLib.emailAuth = {
+
+    // 인증 코드 전송
+    sendCode(email) {
+        const { ajaxLoad } = commonLib;
+        ajaxLoad(`/api/email/auth/${email}`)
+    },
+
+    // 인증 코드 검증
+    verify(authCode) {
+
+    },
+};
