f80548e84cd787936e89080928e243bedf5d56f6
yeonwooda <mcsun5@naver.com>
2024-12-17 15:16:16 +0900

    이메일 전송 기능 구현 (인증)

---
+++ src/main/java/org/koreait/email/exceptions/AuthCodeMismatchException.java
@@ -0,0 +1,12 @@
+package org.koreait.email.exceptions;
+
+import org.koreait.global.exceptions.BadRequestException;
+
+public class AuthCodeMismatchException extends BadRequestException {
+
+    public AuthCodeMismatchException() {
+        super("Mismatch.authCode");
+        setErrorCode(true);
+    }
+
+}
