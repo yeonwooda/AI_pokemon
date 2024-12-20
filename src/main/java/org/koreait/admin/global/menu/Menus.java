d66dcff14adaed423071378a718173acba975b45
yeonwooda <mcsun5@naver.com>
2024-12-20 17:22:21 +0900

    관리자 페이지

---
+++ src/main/java/org/koreait/admin/global/menu/Menus.java
@@ -0,0 +1,36 @@
+package org.koreait.admin.global.menu;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Menus {
+
+    private static Map<String, List<MenuDetail>> submenus;
+
+    static {
+        submenus = new HashMap<>();
+
+        /* 기본 설정 */
+        submenus.put("basic", List.of(
+            new MenuDetail("siteConfig", "사이트 기본정보", "/admin/basic/siteConfig"),
+                new MenuDetail("terms", "약관 관리", "/admin/basic/terms")
+        ));
+
+        // 회원관리
+        submenus.put("member", List.of(
+                new MenuDetail("list", "회원목록", "/admin/member/list")
+        ));
+
+        // 게시판 관리
+        submenus.put("board", List.of(
+           new MenuDetail("list", "게시판목록", "/admin/board/list"),
+           new MenuDetail("add", "게시판 등록", "/admin/board/add"),
+           new MenuDetail("posts", "게시글 관리", "/admin/board/posts")
+        ));
+    }
+
+    public static List<MenuDetail> getMenus(String menuCode) {
+        return submenus.get(menuCode);
+    }
+}
