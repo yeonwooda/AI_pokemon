b88e23d439ba468084e8f7ee7c88a0eb4f320fd2
yeonwooda <mcsun5@naver.com>
2025-01-03 15:11:35 +0900

    .

--- src/main/java/org/koreait/message/controllers/MessageSearch.java
+++ src/main/java/org/koreait/message/controllers/MessageSearch.java
@@ -7,5 +7,7 @@ import java.util.List;
 
 @Data
 public class MessageSearch extends CommonSearch {
-    private List<String> receiver; // 수신쪽 이메일
+    private List<String> sender; // 보낸 쪽 이메일
+    private String mode; // receiver이거나 값이 없으면 받은 쪽지, send : 보낸 쪽지
+
 }
