fe9818ac5527c0fab99879fe2de8c66e1ae5ca41
yeonwooda <mcsun5@naver.com>
2025-01-03 17:12:48 +0900

    .

---
+++ src/main/java/org/koreait/message/services/MessageSendService.java
@@ -0,0 +1,43 @@
+package org.koreait.message.services;
+
+import lombok.RequiredArgsConstructor;
+import org.koreait.file.services.FileDoneService;
+import org.koreait.member.entities.Member;
+import org.koreait.member.exceptions.MemberNotFoundException;
+import org.koreait.member.libs.MemberUtil;
+import org.koreait.member.repositories.MemberRepository;
+import org.koreait.message.constants.MessageStatus;
+import org.koreait.message.controllers.RequestMessage;
+import org.koreait.message.entities.Message;
+import org.koreait.message.repositories.MessageRepository;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+@Lazy
+@Service
+@RequiredArgsConstructor
+public class MessageSendService {
+    private final MemberUtil memberUtil;
+    private final MemberRepository memberRepository;
+    private final MessageRepository repository;
+    private final FileDoneService fileDoneService;
+
+    public void process(RequestMessage form) {
+
+        String email = form.getEmail();
+        Member receiver = !form.isNotice() ? memberRepository.findByEmail(email).orElseThrow(MemberNotFoundException::new) : null;
+
+        Message message = Message.builder()
+                .gid(form.getGid())
+                .notice(form.isNotice())
+                .subject(form.getSubject())
+                .content(form.getContent())
+                .sender(memberUtil.getMember())
+                .receiver(receiver)
+                .status(MessageStatus.UNREAD)
+                .build();
+
+        repository.saveAndFlush(message);
+        fileDoneService.process(form.getGid()); // 파일 업로드 완료 처리
+    }
+}
